f375c6e5f8adeca85c05664e92117803a313c7d4
Ferro Fang <fzinfz@gmail.com>
2017-08-21 15:56:07 +0800

    update registration

--- tsadmin/urls.py
+++ tsadmin/urls.py
@@ -16,9 +16,28 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic.base import RedirectView
+from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url(r'^$', RedirectView.as_view(url='/accounts/login/'), name='index'),
-    url(r'^admin/', admin.site.urls),
-    url(r'^accounts/', include('registration.backends.simple.urls')),
+
+    url(r'^$',
+        TemplateView.as_view(template_name='index.html'),
+        name='index'),
+
+    url(r'^accounts/',
+        include('registration.backends.simple.urls')),
+
+    url(r'^accounts/profile/',
+        TemplateView.as_view(template_name='profile.html'),
+        name='profile'),
+
+    url(r'^login/',
+        auth_views.login,
+        name='login'),
+
+    url(r'^admin/',
+        include(admin.site.urls),
+        name='admin'),
+
 ]
