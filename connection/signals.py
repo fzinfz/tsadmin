0886053ae6df5b3233b8986b94eb949b290e6bef
Ferro Fang <fzinfz@gmail.com>
2018-02-22 23:14:40 +0800

    add post_delete signal

--- connection/signals.py
+++ connection/signals.py
@@ -1,12 +1,13 @@
 from connection.models import Connection,Node
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save,post_delete
 from django.dispatch import receiver
 import requests, sys
 
+@receiver(post_delete, sender=Connection)
 @receiver(post_save, sender=Connection)
-def conn_save(sender, instance, created, **kwargs):
+def conn_save(sender,  **kwargs):
     print(sender, end=" changed: ")
-    print(instance)
+    print(kwargs)
     for node in Node.objects.all():
         try:
             rs = requests.get(node.webhook)
